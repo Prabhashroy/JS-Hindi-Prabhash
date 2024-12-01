4ffb1dd6c48f4699878748eaaf350efa28aa09de
Prabhashroy <130204774+Prabhashroy@users.noreply.github.com>
2024-12-01 06:15:08 +0000

    conversion of datatypes

---
+++ Java_basic/conversionOperation.js
@@ -0,0 +1,32 @@
+let score="Prabhash"
+
+// console.log(typeof score);  //1st methode
+// console.log(typeof(score)); //2nd methode
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+//"33" =>33
+//"3abc"=> 1, false =>0
+
+let isLoggedIn="Prabhash"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false 
+// "" => false
+//"Prabhash" => true
+
+let someNumber =33
+
+let stringNumber= String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
